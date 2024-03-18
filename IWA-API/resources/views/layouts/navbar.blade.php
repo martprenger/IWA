366f80ab6aec22fad76dd53b0ed413767b5d7677
shanos123 <60260010+shanos123@users.noreply.github.com>
2024-03-18 16:14:59 +0100

    new navbas

--- IWA-API/resources/views/layouts/navbar.blade.php
+++ IWA-API/resources/views/layouts/navbar.blade.php
@@ -1,9 +1,27 @@
-<div class="row border border-3 border-dark navbar">
-    <div class="col-md-2 h-100">
-        <img src="{{ asset('images/LogoIWA.jpg') }}" alt="" class="navbar-img">
-    </div>
-    <div class="col-md-5">
 
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">
+                <img src="{{ asset('images/LogoIWA.jpg') }}" alt="Logo" width="30" height="24" class="d-inline-block align-text-top">
+            </a>
+        </div>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Features</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Pricing</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+                </li>
+            </ul>
+        </div>
     </div>
-</div>
+</nav>
 
