713c615bf5c2c314776ad2e8493cf96f17f796ed
aggroenewoud <aggroenewoud@gmail.com>
2024-03-18 15:02:54 +0100

    Add user_role table

---
+++ IWA-API/database/migrations/2024_03_18_124012_create_user_role.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('user_role', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('role_id');
+            $table->timestamps();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('role_id')->references('id')->on('roles');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('your_table_name', function (Blueprint $table) {
+            $table->dropForeign(['user_id']);
+            $table->dropForeign(['role_id']);
+        });
+        Schema::dropIfExists('user_role');
+    }
+};
