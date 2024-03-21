b378b2b2a46a5b564d1b25c30ef9ffb5156888e7
shanos123 <60260010+shanos123@users.noreply.github.com>
2024-03-21 13:13:22 +0100

    made different routes for the different pages with comments

--- IWA-API/routes/web.php
+++ IWA-API/routes/web.php
@@ -21,10 +21,43 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+/*
+ * voor het inloggen op de website
 
+ */
 Route::get('/login', [loginController::class, 'show']);
 Route::post('/custom-login', [loginController::class, 'customLogin'])->name('custom-login');
+
+/*
+ * routes voor de dashboard
+ */
+
 Route::get('/dashboard', [DashboardController::class, 'show']);
+
+/*
+ * routes voor de admin
+ */
+
+Route::get('/admin', [DashboardController::class, 'show']);
+Route::get('/mederwerkersInstellingen', [mederwerkersInstellingenController::class, 'show']);
+Route::get('/logsMederwerkers', [logsMederwerkersController::class, 'show']);
+Route::get('mederwerksToevoegen', [mederwerksToevoegenController::class, 'show']);
+
+
+/*
+ * routes voor de de algemene mederwerks
+ */
+Route::get('/machineToevoegen', [machineToevoegenController::class, 'show']);
 Route::get('/machinePage', [MachineController::class, 'show']);
 Route::get('/machinePage', [MachineController::class, 'getMachines']);
+
+/*
+ * routes voor de administief mederwerks
+ */
+
+Route::get('/factaturen', [factaturenController::class, 'show']);
+Route::get('/lopendeContracten', [lopendeContractenController::class, 'show']);
+
+
+
 Route::get('/testdb', [testdbController::class, 'index']);
