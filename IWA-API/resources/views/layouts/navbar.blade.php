1ab309929d52051120b1ef9588712b5db8b4fbd8
martprenger <mart.prenger@gmail.com>
2024-04-06 15:45:00 +0200

    some changes

--- IWA-API/resources/views/layouts/navbar.blade.php
+++ IWA-API/resources/views/layouts/navbar.blade.php
@@ -1,7 +1,8 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <!-- Left Side Of Navbar -->
     <div class="navbar-brand">
-        <img src="{{ asset('images/LogoIWA.jpg') }}" alt="Logo" width="30" height="24" class="d-inline-block align-text-top">
+        <img src="{{ asset('images/LogoIWA.jpg') }}" alt="Logo" width="30" height="24"
+             class="d-inline-block align-text-top">
     </div>
 
     <!-- Center Side Of Navbar -->
@@ -14,6 +15,21 @@
 
     <!-- Right Side Of Navbar -->
     <div class="navbar-brand">
-        <a class="nav-link disabled" aria-disabled="true">Account</a>
+            <a id="navbarDropdown" class="nav-link" href="#" role="button" data-bs-toggle="dropdown"
+               aria-haspopup="true" aria-expanded="false" v-pre>
+                {{ Auth::user()->name }}
+            </a>
+
+            <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                <a class="dropdown-item" href="{{ route('logout') }}"
+                   onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                    {{ __('Logout') }}
+                </a>
+
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                    @csrf
+                </form>
+            </div>
     </div>
 </nav>
