1305b3b67b28356d3e1ba96a10f3b76bc5c4f990
aggroenewoud <aggroenewoud@gmail.com>
2024-04-16 12:15:23 +0200

    Add contract migrations

---
+++ IWA-API/database/migrations/2024_04_16_100617_create_contracts.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('contracts', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('customer_id');
+            $table->foreign('customer_id')->references('id')->on('klanten')->onDelete('cascade');
+            $table->date('expiration_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('contracts');
+    }
+};
