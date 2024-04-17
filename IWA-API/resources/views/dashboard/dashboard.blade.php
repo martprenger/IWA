2a94bfc7b0e32a80f34b481d8f5673b15b0a4933
The-Big-Fella <jornderoos@hotmail.com>
2024-04-17 15:52:01 +0200

    wallpapers toegevoegd aan de andere accounttypes, machine page design opgefrist en bottom navbar toegevoegd aan de weerstationpagina

--- IWA-API/resources/views/dashboard/dashboard.blade.php
+++ IWA-API/resources/views/dashboard/dashboard.blade.php
@@ -1,7 +1,16 @@
-<div class="row justify-content-center">
-    <div class="col-md-6">
-        <div class="card mt-3">
-            <h1>test</h1>
+<!-- Onscrollbare pagina -->
+<style>body{ overflow:hidden;}</style>
+
+<div class="container-fluid" style="background-image: url('{{ asset('images/wallpaperadmin.jpg') }}'); background-size: cover; height: 100vh;">
+    <div class="text-with-shadow" style="padding-top: 50px; font-family: 'Arial Black' ,serif; font-size: 50px; text-align: center; color: white; text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.5);">WELKOM, NAAM</div>
+    <div class="row justify-content-center align-items-center h-50">
+        <div class="col-md-3 col-sm-3" style="margin-right: 100px;">
+            <div class="text-with-shadow" style="font-family: 'Arial Black' ,serif; font-size: 100px; text-align: center; color: white; text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.5);">500</div>
+            <div class="text-with-shadow" style="font-family: 'Arial Black' ,serif; font-size: 50px; text-align: center; color: white; text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.5);">BINGOKAARTEN</div>
+        </div>
+        <div class="col-md-3 col-sm-3" style="margin-left: 100px;">
+            <div class="text-with-shadow" style="font-family: 'Arial Black',serif; font-size: 100px; text-align: center; color: white; text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.5);">200</div>
+            <div class="text-with-shadow" style="font-family: 'Arial Black',serif; font-size: 50px; text-align: center; color: white; text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.5);">WINNAARS</div>
         </div>
     </div>
 </div>
--- IWA-API/resources/views/dashboard/wetenschapelijk_dashboard.blade.php
+++
@@ -1,7 +0,0 @@
-<div class="row justify-content-center">
-    <div class="col-md-6">
-        <div class="card mt-3">
-            <h1>test</h1>
-        </div>
-    </div>
-</div>
