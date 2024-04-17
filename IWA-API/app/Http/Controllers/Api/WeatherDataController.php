070355ecbb5c586a89c083118de09000478f874c
aggroenewoud <aggroenewoud@gmail.com>
2024-04-17 21:52:22 +0200

    Change error notification + small debugging

--- IWA-API/app/Http/Controllers/Api/WeatherDataController.php
+++ IWA-API/app/Http/Controllers/Api/WeatherDataController.php
@@ -3,25 +3,31 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
 use App\Models\WeatherData;
 use App\Models\Klant;
+use App\Models\Contracten;
+use App\Models\PermissionContract;
+use App\Models\ContractStation;
+use App\Models\APIkeys;
 
 class WeatherDataController extends Controller
 {
 
     public function receiveRequest(Request $request, $id, $station = null) {
-        $this->checkApiContractMatch($request, $apiKey, $id);
+        $this->checkApiContractMatch($request, $id);
         $returnData;
         if (isset($station)) {
             if (Station::where('name', $station)->exists()) {
                 $returnData = $this->getData($this->getQuery($request, $id), [$station]);
             }
         } else {
-            $stations = ContractStations::where('contract_id', $id)->pluck('station')->toArray();
+            $stations = ContractStation::where('contract_id', $id)->pluck('station')->toArray();
             $returnData = $this->getData($this->getQuery($request, $id), $stations);
         }
 
-        return response()->json($returnData, 200);
+        return response()->json($this->getQuery($request, $id), 200);
     }
 
     public function getData($query, $stations) {
@@ -42,14 +48,14 @@ class WeatherDataController extends Controller
     public function getQuery($request, $contractID) {
         $query = array();
         foreach ($request->query() as $key => $val) {
-            if ($key === "value" && WeatherData::isAccesible($val)) {
-                if (ContractPermissions::where("contract_id", $contractID)
+            if ($key == "value" && WeatherData::isAccesible($val)) {
+                if (PermissionContract::where("contract_id", $contractID)
                         ->where("permissions", $val)
                         ->exists()) 
                 {
                     array_push($query, $val);
                 } else {
-                    response()->json(['message' => 'You dont have access to the '.$key.' field'], 401);
+                    abort(401, 'You dont have access to the '.$key.' field');
                 }
             }
         }
@@ -57,15 +63,15 @@ class WeatherDataController extends Controller
             return $query;
         }
 
-        return ContractPermissions::where('contract_id', $contractId)->pluck('permissions')->toArray();
+        return PermissionContract::where('contract_id', $contractID)->pluck('permissions')->toArray();
     }
 
-    public function checkApiContractMatch($request, $apiKey, $contractID) {
-        $this->checkContract($id);
-        $this->checkApiKey($request);
+    public function checkApiContractMatch($request, $contractID) {
+        $this->checkContract($contractID);
+        $apiKey = $this->checkApiKey($request);
         
         $getApiCustomerId = APIkeys::where("APIkey", $apiKey)->value("klantenID");
-        $getContractCustomerId = Contracts::where('id', $contractID)->value("customer_id");
+        $getContractCustomerId = Contracten::where('id', $contractID)->value("customer_id");
 
         return ($getApiCustomerId == $getContractCustomerId);
     }
@@ -75,17 +81,19 @@ class WeatherDataController extends Controller
         if ($request->has("api-key")) {
             $apiKey = $request->input("api-key");
         } else {
-            return response()->json(['message' => 'No API key provided'], 401);
+            abort(401, 'No API key provided');
         }
 
-        if (!APIkeys::where("APIkey", $apiKey)->exists()) {
-            return response()->json(['message' => 'Non existant API key provided'], 401);
+        if (APIkeys::where("APIkey", $apiKey)->exists()) {
+            return $apiKey;
+        } else {
+            abort(401, 'Non existant API key provided');
         }
     }
 
     public function checkContract($id) {
-        if (!Contracts::where("id", $id)->exists()) {
-            return response()->json(['message' => 'Non existant contract'], 401);
+        if (!Contracten::where("id", $id)->exists()) {
+            abort(401, 'Non existant contract');
         }
     }
 }
