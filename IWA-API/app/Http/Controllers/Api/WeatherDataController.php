e522c7545bec0925253969f52bbaf5adf6c83d4b
aggroenewoud <aggroenewoud@gmail.com>
2024-04-18 16:02:35 +0200

    Change return json stucture and add a count functionality

--- IWA-API/app/Http/Controllers/Api/WeatherDataController.php
+++ IWA-API/app/Http/Controllers/Api/WeatherDataController.php
@@ -19,33 +19,41 @@ class WeatherDataController extends Controller
     public function receiveRequest(Request $request, $id, $station = null) {
         $this->checkApiContractMatch($request, $id);
         $returnData;
+        $count = $this->getCount($request);
         if (isset($station)) {
             if (Station::where('name', $station)->exists()) {
-                $returnData = $this->getData($this->getQuery($request, $id), [$station]);
+                $returnData = $this->getData($this->getQuery($request, $id), [$station], $count);
             }
         } else {
             $stations = ContractStation::where('contract_id', $id)->pluck('station')->toArray();
-            $returnData = $this->getData($this->getQuery($request, $id), $stations);
+            $returnData = $this->getData($this->getQuery($request, $id), $stations, $count);
         }
 
         return response()->json($returnData, 200);
     }
 
-    public function getData($query, $stations) {
+    public function getCount($request) {
+        if ($request->has("count")) {
+            return (int) $request->input("count");
+        } else {
+            return 1;
+        }
+    }
+    
+    public function getData($query, $stations, $count) {
         $returnData = array();
         
         foreach ($stations as $station) {
+            $queryBuilder = WeatherData::query();
+            $queryBuilder->select($query)->where("STN", $station)->latest()->take($count);
             $stationSpecificData = array();
-            $stationSpecificData["STN"] = $station;
-            foreach ($query as $type) {
-                $stationSpecificData[$type] = WeatherData::where("STN", $station)->latest()->value($type);
-            }
+            $stationSpecificData[$station] = $queryBuilder->get();
             array_push($returnData, $stationSpecificData);
         }
 
         return $returnData;
     }
-    
+
     public function getQuery($request, $contractID) {
         $query = array();
         $values = explode(",", $request->query('value'));
