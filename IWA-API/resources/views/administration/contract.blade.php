1ff76c1678b00f32c71263042f3dbdd10896529b
The-Big-Fella <jornderoos@hotmail.com>
2024-04-18 13:33:16 +0200

    navbar toegevoegd filters verplaatst

--- IWA-API/resources/views/administration/contract.blade.php
+++ IWA-API/resources/views/administration/contract.blade.php
@@ -7,9 +7,13 @@
         <nav class="navbar navbar-expand-lg navbar-light bg-rgba" style="background-color: rgba(77, 153, 231, 1);">
             <!-- Left Side Of Navbar -->
             <div class="navbar-brand" style="margin-left: 35px;">
-                <a href="{{ route('addstation') }}"><svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="rgba(173, 255, 47, 1)" class="bi bi-plus-circle-fill" viewBox="0 0 16 16">
+                <a href="{{ route('addcontract') }}"><svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="rgba(173, 255, 47, 1)" class="bi bi-plus-circle-fill" viewBox="0 0 16 16">
                         <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3z"/>
                     </svg></a>
+                <a href="{{ route('addemployees') }}" style="margin-left: 15px;"><svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="rgba(173, 255, 47, 1)" class="bi bi-person-plus-fill" viewBox="0 0 16 16">
+                        <path d="M1 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6"/>
+                        <path fill-rule="evenodd" d="M13.5 5a.5.5 0 0 1 .5.5V7h1.5a.5.5 0 0 1 0 1H14v1.5a.5.5 0 0 1-1 0V8h-1.5a.5.5 0 0 1 0-1H13V5.5a.5.5 0 0 1 .5-.5"/>
+                    </svg></a> <!-- om klanten toe te voegen, moet nog bijgewerkt worden -->
 
             </div>
             <!-- Center Side Of Navbar -->
@@ -19,7 +23,7 @@
 
             <!-- Right Side Of Navbar -->
             <div class="navbar-search" style="margin-right: -60px; margin-top:15px;">
-                <form method="POST" action="{{ route('medewerkers') }}">
+                <form method="POST" action="{{ route('contracten') }}">
                     @csrf
                     <div class="row justify-content-end">
 
@@ -27,7 +31,7 @@
                                 <input type="text" name="id" class="form-control" placeholder="ID">
                             </div>
                             <div class="col-md-2">
-                                <input type="text" name="klantenNaam" class="form-control" placeholder="Klant naam">
+                                <input type="text" name="klantenNaam" class="form-control" placeholder="Klantnaam">
                             </div>
                             <div class="col-md-2">
                                 <input type="text" name="aantalstations" class="form-control" placeholder="Aantal stations">
@@ -56,7 +60,7 @@
                     <th>klant naam</th>
                     <th>Aantal stations</th>
                     <th>Aantal permisions</th>
-                    <th><a href="{{ route('addcontract') }}" class="btn btn-success">make new contract</a></th>
+                    <th></th>
                 </tr>
                 </thead>
 
@@ -70,14 +74,18 @@
                         <td>
                             <!--TODO: make styling not use -15 px, i know this is bad but i hate front end-->
                             <div
-                                style="display: flex; align-items: center; margin-bottom: -20px; margin-top: -20px;">
+                                style="display: flex; align-items: center; margin-bottom: -20px; margin-top: -20px;; margin-right: -72px;">
                                 <a href="{{ route('editcontract', ['id' => $contract->id]) }}"
-                                   class="btn btn-primary" style="margin-right: 5px;">Edit</a>
+                                   class="btn btn-primary" style="margin-right: -20px;"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil" viewBox="0 0 16 16">
+                                        <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325"/>
+                                    </svg></a><!-- Edit -->
                                 <form method="POST" action="{{ route('deletecontract') }}"
-                                      style="margin-top: 4px; margin-left: -15px">
+                                      style="margin-top: 4px; margin-left: 16px">
                                     @csrf
                                     <input type="hidden" name="id" value="{{ $contract->id }}">
-                                    <button type="submit" class="btn btn-danger">Delete</button>
+                                    <button type="submit" class="btn btn-danger"><svg xmlns="http://www.w3.org/2000/svg" width="17" height="17" fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16">
+                                            <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5M11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47M8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5"/>
+                                        </svg></button><!-- Remove -->
                                 </form>
                             </div>
                         </td>
@@ -87,32 +95,6 @@
             </table>
         </div>
     </div>
-    <div class="container">
-        <form method="POST" action="{{ route('APIManagements') }}">
-            @csrf
-            <div class="row">
-                <div class="col-md-2">
-                    <input type="text" name="id" class="form-control" placeholder="ID">
-                </div>
-                <div class="col-md-2">
-                    <input type="text" name="klantenNaam" class="form-control" placeholder="Klant naam">
-                </div>
-                <div class="col-md-2">
-                    <input type="text" name="aantalstations" class="form-control" placeholder="Aantal stations">
-                </div>
-                <div class="col-md-2">
-                    <input type="text" name="aantalpermisions" class="form-control" placeholder="Aantal permisions">
-                </div>
-                <div class="col-md-2">
-                    <button type="submit" class="btn btn-primary">Filter</button>
-                </div>
-            </div>
-        </form>
-    </div>
-
-
-
-
 
 @endsection
 
